2b88811ac75d65c6c5f6a13a7c999ae0dd6b4898
Tomasz Zabawka <tomasz.zbwk@gmail.com>
2021-10-06 11:33:47 +0200

    refactor: trying to leverage connection pooling

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -6,16 +6,26 @@ import {
   TypeOrmModuleOptions,
   TypeOrmOptionsFactory,
 } from '@nestjs/typeorm';
+
 import { databaseConfig } from 'src/config/database.config';
+
+enum Connections {
+  POC = 'poc',
+  POC_DEMO = 'poc_demo',
+}
 
 export class CustomTypeOrmOptionsFactory implements TypeOrmOptionsFactory {
   constructor(@Inject(REQUEST) private readonly request: Request) {}
   createTypeOrmOptions(): TypeOrmModuleOptions | Promise<TypeOrmModuleOptions> {
     const defaultOpts = databaseConfig;
 
-    return this.request.headers['demo-mode'] === 'false'
-      ? ({ ...defaultOpts, name: 'poc' } as any)
-      : { ...defaultOpts, database: 'poc_demo', name: 'poc_demo' };
+    return this.request.headers?.['demo-mode'] === 'false'
+      ? ({ ...defaultOpts, name: Connections.POC } as any)
+      : {
+          ...defaultOpts,
+          database: Connections.POC_DEMO,
+          name: Connections.POC_DEMO,
+        };
   }
 }
 @Module({
